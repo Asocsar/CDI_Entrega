32063ea84994e3a3cf88cebc56f950ac2ea7fb5a
ericgs230898 <ericgs230898@gmail.com>
2021-05-13 10:59:40 +0200

    Add time

--- Codigo.py
+++ Codigo.py
@@ -2,6 +2,7 @@ import argparse
 import os.path
 import LZ4_Rate, LZ4_Time, LZ4_Transmission
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--file', default=None, required=True, help='File to Compress or Decompress')
@@ -10,10 +11,14 @@ if __name__ == "__main__":
     parser.add_argument('--mode', default=0, type=int, required=False, help='0 for optimizing Compression Ratio, \
                                                                              1 for optimizing Time of Compression, \
                                                                              2 for optimizing time of Transimision')
+    parser.add_argument('--VA', default=150, type=int, required=False, help='Ventana Avanzada')
+    parser.add_argument('--VB', default=65536, type=int, required=False, help='Ventanza Búsqueda')
     args = parser.parse_args()
     filename = args.file
     CorD = args.Compress_Decompress
     mode = args.mode
+    va = args.VA
+    vb = args.VB
 
     assert os.path.isfile(filename), "File path is wrong or it does not exist"
     assert mode <=2, "Mode should be equal or least than 2"
@@ -26,7 +31,7 @@ if __name__ == "__main__":
             Decompressed = LZ4_Rate.decompress(filename)
     elif mode == 1:
         if CorD == 0:
-            Compressed = LZ4_Time.compress(filename)
+            Compressed = LZ4_Time.compress(filename, va, vb)
         else:
             Decompressed = LZ4_Time.decompress(filename)
     else:
